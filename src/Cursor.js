f8fd0080f9c6fd1bcbef202f3488deae87a5ec0b
Andrew Crites <acrites@mobiquityinc.com>
2013-12-27 10:43:26 -0500

    Rewriting Cursor to implement MongoCursor API

--- src/Cursor.js
+++ src/Cursor.js
@@ -1,12 +1,25 @@
-Cursor = function (cursor, mongoise) {
+var methods,
+    Cursor = function (cursor, mongoise) {
     this.cursor = cursor;
     this.mongoise = mongoise;
 };
 
-Cursor.prototype = {
-    toArray: function () {
+methods = ["rewind", "stream", "isClosed"];
+methods.forEach(function (func) {
+    Cursor.prototype[func] = function () {
+        return this.cursor.apply(this.cursor, arguments);
+    };
+});
+
+methods = ["toArray", "each", "count", "sort", "limit", "maxTimeMS",
+"setReadPreference", "skip", "batchSize", "nextObject", "explain", "close"];
+
+methods.forEach(function (func) {
+    Cursor.prototype[func] = function () {
         var dfd = new this.mongoise.Deferred;
-        this.cursor.toArray(function (err, result) {
+
+        args = Array.prototype.slice.call(arguments);
+        args.push(function (err, result) {
             if (err) {
                 dfd.reject(err);
             }
@@ -15,8 +28,10 @@ Cursor.prototype = {
             }
         });
 
+        this.cursor[func].apply(this.cursor, args);
+
         return dfd.promise;
-    }
-}
+    };
+});
 
 module.exports = Cursor;
